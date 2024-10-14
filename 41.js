28b0132f83615ff87e105dbc5dd79920c918ee4e
Andrewdragom <andreasdragomirov@yandex.com>
2024-10-14 19:13:47 +0300

    Add files via upload

---
+++ 41.js
@@ -0,0 +1,5 @@
+// Only change code below this line
+const myArray = [
+  [1, "df"],
+  [2, "sf"],
+];
---
+++ 49.js
@@ -0,0 +1,6 @@
+const myList = [];
+myList.push(["Chocolat", 35]);
+myList.push(["Chocolatka", 59]);
+myList.push(["Chip", 56]);
+myList.push(["Cheese", 32]);
+myList.push(["Chomber", 33]);
